8a72bdbe693af1412b61185e903ef628ccefb98a
Ivan Mendoza <imendoza9691@gmail.com>
2018-03-18 15:10:29 -0400

    #2 created business table

--- server/db/rasDB.sql
+++ server/db/rasDB.sql
@@ -4,7 +4,7 @@ CREATE DATABASE rasDB;
 \c rasDB;
 
 CREATE TABLE users (
-  ID SERIAL PRIMARY KEY,
+  userID SERIAL PRIMARY KEY,
   email VARCHAR (355) UNIQUE NOT NULL,
   password_digest VARCHAR (50) NOT NULL,
   created_at TIMESTAMP NOT NULL
@@ -12,3 +12,16 @@ CREATE TABLE users (
 
 INSERT INTO users (email, password_digest, created_at)
   VALUES ('user_email@example.com', 'password', now()),('stephaniehou@ac.c4q.nyc','stephaniehou', now()),('angelarias@ac.c4q.nyc', 'angelarias', now()),('susantan@ac.c4q.nyc','susantan', now()), ('ivanmendoza@ac.c4q.nyc', 'ivanmendoza', now());
+
+CREATE TABLE businesses (
+  businessID SERIAL PRIMARY KEY,
+  email VARCHAR (355) UNIQUE NOT NULL,
+  first_name VARCHAR (50) NOT NULL,
+  last_name VARCHAR (50) NOT NULL,
+  password_digest VARCHAR (50) NOT NULL,
+  created_at TIMESTAMP NOT NULL
+);
+
+INSERT INTO users (email, first_name, last_name, password_digest, created_at)
+  VALUES ('stephaniehou@ac.c4q.nyc', 'Stephanie', 'Hou', 'stephaniehou', now()),('angelarias@ac.c4q.nyc', 'Angel' ,'Arias', 'angelarias', now()),('susantan@ac.c4q.nyc', 'Susan' , 'Tan', 'susantan', now()), ('ivanmendoza@ac.c4q.nyc', 'Ivan', 'Mendoza', 'ivanmendoza', now()); 
+
