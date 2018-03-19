e7af6e61651cffb379524343c61187e9cf915e8a
Ivan Mendoza <imendoza9691@gmail.com>
2018-03-19 19:18:01 -0400

    #4 added controller for index route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const indexController = require("../controllers/index");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  const renderObject = {};
+  renderObject.title = "Welcome to Express!";
+  indexController.sum(1, 2, (error, results) => {
+    if (error) return next(error);
+    if (results) {
+      renderObject.sum = results;
+      res.render("index", renderObject);
+    }
+  });
 });
 
 module.exports = router;
