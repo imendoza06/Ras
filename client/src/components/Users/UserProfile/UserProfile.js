3af9ed1533c7037e1107d17456d0b68862d5cf42
Susan Tan <susan.tan198@gmail.com>
2018-04-07 16:44:24 -0400

    and again updating

--- client/src/components/Users/UserProfile/UserProfile.js
+++ client/src/components/Users/UserProfile/UserProfile.js
@@ -21,7 +21,7 @@ const Account = e => {
           <br />
           <div id="profilebox">
             <h3 className="h3divs">User Information</h3>
-            <br/>
+            <br />
             <strong>Username:</strong> DeborahSmith123
             <br />
             <br /> <strong>Password:</strong> *******
@@ -38,7 +38,11 @@ const Account = e => {
           </div>
           <br />
           <div id="bottombox">
-            <br /><h3 className="h3divs"> <strong>Reviews of Deborah: </strong></h3>
+            <br />
+            <h3 className="h3divs">
+              {" "}
+              <strong>Reviews of Deborah: </strong>
+            </h3>
             <br />
             <ul>
               <li>
@@ -91,32 +95,32 @@ const Settings = e => {
       <div class="profileheads">
         <h3>Settings</h3>
         <div id="editprofile">
-        <h3 className="h3divs">Edit Profile</h3>
-        <br />First Name <input type="text" className="inputs" />
-        <br />
-        <br />Last Name <input type="text" className="inputs" />
-        <br />
-        <br />I Am{" "}
-        <select>
-          <option>female</option>
-          <option>male</option>
-          <option>other</option>
+          <h3 className="h3divs">Edit Profile</h3>
+          <br />First Name <input type="text" className="inputs" />
           <br />
-        </select>
-        <br />
-        <br />
-        Birthday <br /> <input type="date" name="bday" />
-        <br />
-        <br />Email Address <input type="text" className="inputs" />
-        <br />
-        <br />Phone Number <input type="text" className="inputs" />
-        <br />
-        <br />Where You Live <input type="text" className="inputs" />
-        <br />
-        <br />Describe Yourself <br />
-        <br />
-        <textarea id="textarea">Tell us about you!</textarea>
-      </div>
+          <br />Last Name <input type="text" className="inputs" />
+          <br />
+          <br />I Am{" "}
+          <select>
+            <option>female</option>
+            <option>male</option>
+            <option>other</option>
+            <br />
+          </select>
+          <br />
+          <br />
+          Birthday <br /> <input type="date" name="bday" />
+          <br />
+          <br />Email Address <input type="text" className="inputs" />
+          <br />
+          <br />Phone Number <input type="text" className="inputs" />
+          <br />
+          <br />Where You Live <input type="text" className="inputs" />
+          <br />
+          <br />Describe Yourself <br />
+          <br />
+          <textarea id="textarea">Tell us about you!</textarea>
+        </div>
       </div>
     </div>
   );
@@ -159,81 +163,83 @@ const Favorites = e => {
       <div class="profileheads">
         <h3>Favorties</h3>
         <div id="favorites">
-        <h3 className="h3divs"><strong>My Ultimate Studios </strong></h3>
-        <ul>
-          <li>
-            <strong>Empire Rehearsal: </strong>
+          <h3 className="h3divs">
+            <strong>My Ultimate Studios </strong>
+          </h3>
+          <ul>
+            <li>
+              <strong>Empire Rehearsal: </strong>
+              <br />
+              5 Stars: It was great, the studio was prepared and ready for me.
+            </li>
             <br />
-            5 Stars: It was great, the studio was prepared and ready for me.
-          </li>
-          <br />
-          <li>
-            <strong>Ripley-Grier Studios</strong>
+            <li>
+              <strong>Ripley-Grier Studios</strong>
+              <br />
+              1 Stars: The studio smelled like cheese and cats! No bueno.
+            </li>
             <br />
-            1 Stars: The studio smelled like cheese and cats! No bueno.
-          </li>
-          <br />
-          <li>
-            <strong>Brooklyn Studios For Dance</strong>
+            <li>
+              <strong>Brooklyn Studios For Dance</strong>
+              <br />
+              4 Stars: It was great, the studio was prepared and ready for me.
+            </li>
             <br />
-            4 Stars: It was great, the studio was prepared and ready for me.
-          </li>
-          <br />
-          <li>
-            <strong>Millennium Dance Complex </strong>
+            <li>
+              <strong>Millennium Dance Complex </strong>
+              <br />
+              5 Stars: It was great, the studio was prepared and ready for me.
+            </li>
             <br />
-            5 Stars: It was great, the studio was prepared and ready for me.
-          </li>
-          <br />
-          <li>
-            <strong>Worldwide Premiere Studio</strong>
+            <li>
+              <strong>Worldwide Premiere Studio</strong>
+              <br />
+              3 Stars: It was great, the studio was prepared and ready for me.
+            </li>
             <br />
-            3 Stars: It was great, the studio was prepared and ready for me.
-          </li>
-          <br />
-          <li>
-            <strong>Dance Dance</strong>
+            <li>
+              <strong>Dance Dance</strong>
+              <br />
+              4 Stars: It was great, the studio was prepared and ready for me.
+            </li>
             <br />
-            4 Stars: It was great, the studio was prepared and ready for me.
-          </li>
-          <br />
-          <li>
-            <strong>Fred Astaire Dance </strong>
+            <li>
+              <strong>Fred Astaire Dance </strong>
+              <br />
+              5 Stars: It was great, the studio was prepared and ready for me.
+            </li>
             <br />
-            5 Stars: It was great, the studio was prepared and ready for me.
-          </li>
-          <br />
-          <li>
-            <strong>Sessions Dance</strong>
+            <li>
+              <strong>Sessions Dance</strong>
+              <br />
+              1 Stars: The studio smelled like cheese and cats! No bueno.
+            </li>
             <br />
-            1 Stars: The studio smelled like cheese and cats! No bueno.
-          </li>
-          <br />
-          <li>
-            <strong>Rainbow Dancers </strong>
+            <li>
+              <strong>Rainbow Dancers </strong>
+              <br />
+              5 Stars: It was great, the studio was prepared and ready for me.
+            </li>
             <br />
-            5 Stars: It was great, the studio was prepared and ready for me.
-          </li>
-          <br />
-          <li>
-            <strong>Mercury</strong>
+            <li>
+              <strong>Mercury</strong>
+              <br />
+              1 Stars: The studio smelled like cheese and cats! No bueno.
+            </li>
             <br />
-            1 Stars: The studio smelled like cheese and cats! No bueno.
-          </li>
-          <br />
-          <li>
-            <strong>Empire Rehearsal </strong>
+            <li>
+              <strong>Empire Rehearsal </strong>
+              <br />
+              5 Stars: It was great, the studio was prepared and ready for me.
+            </li>
             <br />
-            5 Stars: It was great, the studio was prepared and ready for me.
-          </li>
-          <br />
-          <li>
-            <strong>Champions</strong>
+            <li>
+              <strong>Champions</strong>
+              <br />
+              1 Stars: The studio smelled like cheese and cats! No bueno.
+            </li>
             <br />
-            1 Stars: The studio smelled like cheese and cats! No bueno.
-          </li>
-          <br />
-        </ul>
+          </ul>
         </div>
       </div>
     </div>
@@ -245,82 +251,85 @@ const Reviews = e => {
     <div class="rightdiv">
       <div class="profileheads">
         <h3>Reviews</h3>
-        <div id = "reviews">
-        <h3 className="h3divs"><strong>Deborah's Studio Reviews </strong></h3>
-        <ul>
-          <li>
-            <strong>Penthouse: </strong>
+        <div id="reviews">
+          <h3 className="h3divs">
+            <strong>Deborah's Studio Reviews </strong>
+          </h3>
+          <ul>
+            <li>
+              <strong>Penthouse: </strong>
+              <br />
+              5 Stars: It was great, the studio was prepared and ready for me.
+            </li>
             <br />
-            5 Stars: It was great, the studio was prepared and ready for me.
-          </li>
-          <br />
-          <li>
-            <strong>Mercury</strong>
+            <li>
+              <strong>Mercury</strong>
+              <br />
+              1 Stars: The studio smelled like cheese and cats! No bueno.
+            </li>
             <br />
-            1 Stars: The studio smelled like cheese and cats! No bueno.
-          </li>
-          <br />
-          <li>
-            <strong>Dance Dance</strong>
+            <li>
+              <strong>Dance Dance</strong>
+              <br />
+              4 Stars: Deborah was great to work with and friendly
+            </li>
             <br />
-            4 Stars: Deborah was great to work with and friendly
-          </li>
-          <br />
-          <li>
-            <strong>Penthouse: </strong>
+            <li>
+              <strong>Penthouse: </strong>
+              <br />
+              5 Stars: Deborah was a pleasure and left the studio in great
+              shape.
+            </li>
             <br />
-            5 Stars: Deborah was a pleasure and left the studio in great shape.
-          </li>
-          <br />
-          <li>
-            <strong>Mercury</strong>
+            <li>
+              <strong>Mercury</strong>
+              <br />
+              3 Stars: Deborah overstayed but danced great
+            </li>
             <br />
-            3 Stars: Deborah overstayed but danced great
-          </li>
-          <br />
-          <li>
-            <strong>Dance Dance</strong>
+            <li>
+              <strong>Dance Dance</strong>
+              <br />
+              4 Stars: Deborah was great to work with and friendly
+            </li>
             <br />
-            4 Stars: Deborah was great to work with and friendly
-          </li>
-          <br />
-          <li>
-            <strong>Penthouse: </strong>
+            <li>
+              <strong>Penthouse: </strong>
+              <br />
+              5 Stars: It was great, the studio was prepared and ready for me.
+            </li>
             <br />
-            5 Stars: It was great, the studio was prepared and ready for me.
-          </li>
-          <br />
-          <li>
-            <strong>Mercury</strong>
+            <li>
+              <strong>Mercury</strong>
+              <br />
+              1 Stars: The studio smelled like cheese and cats! No bueno.
+            </li>
             <br />
-            1 Stars: The studio smelled like cheese and cats! No bueno.
-          </li>
-          <br />
-          <li>
-            <strong>Penthouse: </strong>
+            <li>
+              <strong>Penthouse: </strong>
+              <br />
+              5 Stars: It was great, the studio was prepared and ready for me.
+            </li>
             <br />
-            5 Stars: It was great, the studio was prepared and ready for me.
-          </li>
-          <br />
-          <li>
-            <strong>Mercury</strong>
+            <li>
+              <strong>Mercury</strong>
+              <br />
+              1 Stars: The studio smelled like cheese and cats! No bueno.
+            </li>
             <br />
-            1 Stars: The studio smelled like cheese and cats! No bueno.
-          </li>
-          <br />
-          <li>
-            <strong>Penthouse: </strong>
+            <li>
+              <strong>Penthouse: </strong>
+              <br />
+              5 Stars: It was great, the studio was prepared and ready for me.
+            </li>
             <br />
-            5 Stars: It was great, the studio was prepared and ready for me.
-          </li>
-          <br />
-          <li>
-            <strong>Mercury</strong>
+            <li>
+              <strong>Mercury</strong>
+              <br />
+              1 Stars: The studio smelled like cheese and cats! No bueno.
+            </li>
             <br />
-            1 Stars: The studio smelled like cheese and cats! No bueno.
-          </li>
-          <br />
-        </ul>
+          </ul>
         </div>
       </div>
     </div>
